dc67eadd38735213541f7ac01e4dd5b3bea91862
Vladyslav Penkin <vy891078@gmail.com>
2024-06-09 22:50:30 +0300

    updated pnpm dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: ['react-refresh', 'prefer-arrow', 'i18next'],
 	rules: {
+	  'max-warnings': 'off',
 		'react/react-in-jsx-scope': 'off',
 		'react-hooks/exhaustive-deps': 'off',
 		'prettier/prettier': 'warn',
